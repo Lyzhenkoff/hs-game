f98f5aca651e74e8057e00900e888cd8ed6e28d2
Кир Кирыч <kirilllyzenkov@MacBook-Pro-Kir.local>
2026-02-25 10:25:18 +0300

    fix prisma import

--- app/admin/media/page.tsx
+++ app/admin/media/page.tsx
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 import MediaForm from "./ui";
 
 export default async function AdminMediaPage() {
--- app/registrations/page.tsx
+++ app/registrations/page.tsx
@@ -1,5 +1,4 @@
-import { prisma } from "@/lib/prisma";
-
+import prisma from "@/lib/prisma";
 export default async function AdminRegistrationsPage() {
     const events = await prisma.event.findMany({
         orderBy: { createdAt: "desc" },
