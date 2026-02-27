83d9dabb1aa5ab5b92082e8d6151f10eb27e4b3e
Кир Кирыч <kirilllyzenkov@MacBook-Pro-Kir.local>
2026-02-27 15:42:13 +0300

    add vercel speed insights

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           <CalendarPoster />
               <FactionsSection />
               <TestimonialsSection />
-              <GallerySection />
+              {/*<GallerySection />*/}
           </AnimatedSection>
           <RequestGameForm />
 
