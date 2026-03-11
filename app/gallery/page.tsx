cba4c075fd7da89897babf32353049419077c0ec
Кир Кирыч <kirilllyzenkov@MacBook-Pro-Kir.local>
2026-03-11 15:26:34 +0300

    add social links

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -20,14 +20,14 @@ const photos = [
     "/gallery/photo18.JPG",
     "/gallery/photo19.JPG",
     "/gallery/photo20.JPG",
-    "/gallery/photo21.jpg",
+    "/gallery/photo21.JPG",
     "/gallery/photo22.JPG",
     "/gallery/photo23.JPG",
     "/gallery/photo24.JPG",
     "/gallery/photo25.JPG",
     "/gallery/photo27.JPG",
-    "/gallery/photo28.jpg",
-    "/gallery/photo29.jpg",
+    "/gallery/photo28.JPG",
+    "/gallery/photo29.JPG",
 ];
 export const metadata = {
     title: "Галерея — Ход Судьбы",
